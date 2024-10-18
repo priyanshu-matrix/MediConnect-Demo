a1200305369aeda9d8f93734e650df0325a10af2
Priyanshu Kumar Pandey <priyanshupandey44449@gmail.com>
2024-10-18 12:55:09 +0530

    creating a new branch

--- Backend/utils/extras.py
+++ Backend/utils/extras.py
@@ -1,6 +1,11 @@
 import random
 from string import digits
+def greet():
+    print("welcome")
 
-
+@greet()
 def get_random_id(length: int) -> int:
+
+
     return "".join(random.choices(digits, k=length))
+    
